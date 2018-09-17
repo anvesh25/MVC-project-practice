53a0ba0b66a723b449c16c959401f0092bfe073b
anvesh25 <anveshbabu.26@gmail.com>
2018-09-17 23:44:15 +0530

    Adding Interface repositories for project

---
+++ DrinkApplication/src/DrinkApplication/Data/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DrinkApplication.Data.Models;
+
+namespace DrinkApplication.Data.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> Categories { get; set; }
+    }
+}
---
+++ DrinkApplication/src/DrinkApplication/Data/Interfaces/IDrinkRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DrinkApplication.Data.Models;
+
+namespace DrinkApplication.Data.Interfaces
+{
+    public interface IDrinkRepository
+    {
+       IEnumerable<Drink> Drinks { get; set; }
+        IEnumerable<Drink> PreferrencesDrinks { get; set; }
+        Drink GetDrinkById(int drinkId);
+
+    }
+}
